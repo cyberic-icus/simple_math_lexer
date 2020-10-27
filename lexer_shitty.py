e6404c7047dd18d86490a1870e57ae4a0ff1a3b5
Michael <58913963+cyberic-icus@users.noreply.github.com>
2020-10-27 23:25:40 +0300

    Update lexer_shitty.py

--- lexer_shitty.py
+++ lexer_shitty.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
+"""
+Программа представляет собой простой калькулятор.
+																Она состоит из 2 частей:
+1) Упрощение исходного выражения в более простой вид.								| 2) Вычисление значения преобразованного математического выражения.
+	В этой части функция lex преобразует входное выражение в удобную для работы		|	  Для вычисления значения используется алгоритм, похожий на алгоритм рекурсивного спуска.
+строку, которую потом примет в качестве аргумента функция analize. 					|
+
+Реализация алгоритма вычисления выражения отличается от стандартного алгоритма рекурсивного спуска. Доку допишу завтра.
+
 
 """
-Простой лексический анализатор. (мне лень было писать доку)
-дрисня полная если честно, без токенизации и явной последовательности 
-выполнения операторов.
+import math
+import sys
 
-"""
-print("Доступные операции: +,-,*,/.")
-expression = str(input("Введите математическое выражение: ")) or '2*2-4/2-1'
 
 
 def sum(a,b): return a + b
@@ -17,6 +22,10 @@ def div(a,b):
 	if b != 0: return a / b
 	else: return None
 
+def cos(a): return math.cos(a)
+def sin(a): return math.sin(a)
+def tg(a): return math.tan(a)
+def ctg(a): return math.atan(a)
 
 
 def lex(exp):
@@ -49,6 +58,8 @@ def lex(exp):
 
 		# Функция find возвращает -1 при отсутствии оператора, иначе индекс
 		try:
+			# ===================================================================== #
+			# Реализация рекурсивного спуска для бинарных операторов 
 			sum_index = exp.find('+')
 			if sum_index >-1: return sum(lex(exp[:sum_index]), lex(exp[sum_index+1:]))
 			
@@ -60,30 +71,61 @@ def lex(exp):
 
 			div_index = exp.find('/')
 			if div_index >-1: return div(lex(exp[:div_index]), lex(exp[div_index+1:]) )
+
+			# ===================================================================== #
+			# Реализация рекурсивного спуска для функций 
+			sin_index = exp.find('#')
+			if sin_index >-1: return sin(lex(str(exp[sin_index+1:len(exp)])))
+
+			cos_index = exp.find('$')
+			if cos_index >-1: return cos(lex(exp[cos_index+1:len(exp)]))
+
+			tg_index = exp.find('@')
+			if tg_index >-1: return tg(lex(exp[tg_index+1:len(exp)]))
+
+			ctg_index = exp.find('!')
+			if ctg_index >-1: return ctg(lex(exp[ctg_index+1:len(exp)]))
+
+			# ===================================================================== #
+			if type(float(exp)) is float or type(exp) is int: 
+				return str(exp)
+
 		except Exception as e:
 			pass
 
+
+
+	exp = exp.replace('sin', '#')
+	exp = exp.replace('cos', '$')
+	exp = exp.replace('tg', '@')
+	exp = exp.replace('ctg', '!')
+
+	exp = exp.replace('pi', f'{math.pi}')
+	exp = exp.replace('e', f'{math.e}')
+	
+
 	try: # Если строка - число, преобразуем тип и возвращаем значение.
 		exp = float(exp)
-
 		return exp
 	except Exception as e:
-
-		try: # тут мы находим все скобки, вычисляем их значения и вставляем в исходное выражение
-			for i in range(exp.count('(')): 
-				braces_indexes = [exp.rfind('('), exp.find(')')] # находим внутренние скобки
+		try: # тут мы находим все скобки, вычисляем их значения и вставляем в исходное выражение print(f'{}')
+			for _ in range(exp.count('(')): 
+				braces_indexes = [exp.rfind('('), exp.find(')', exp.rfind('('))] # находим внутренние скобки
 				if braces_indexes[0] > -1 and braces_indexes[1] > -1:
 					braced_exp = exp[braces_indexes[0]+1:braces_indexes[1]]
-					# print(exp) разкомменть для подробного разбора
 					exp = exp[:braces_indexes[0]] + str(analize(braced_exp)) + exp[braces_indexes[1]+1:] # Склеиваем
-					# print(exp) разкомменть для подробного разбора
+					
 			
 			return analize(exp)
 
 		except Exception as e:
 			pass # Выдает вполне логичный результат при эксепшене
 
-result = lex(expression)
-print(f"{expression} = {result}")
 
-input("Введите любую клавишу для выхода.")
+
+if __name__ == '__main__':
+	print("Доступные операции: +,-,*,/.")
+	expression = sys.argv[1] or str(input()) # Можно сделать переменное число аргументов
+	result = lex(expression)
+	print(f"{expression} = {result}")
+
